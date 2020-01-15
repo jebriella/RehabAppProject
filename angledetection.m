abde0a31a8588c730a6513f6a3d974bd14cbf87f
Simon <simon.w.mogren@gmail.com>
2020-01-15 22:26:05 +0100

    moved matlab code into separate directory

--- angledetection.m
+++
@@ -1,9 +0,0 @@
-function NEWANGLE = angledetection(EUL1,EUL2,Eulzero1,Eulzero2)
-% Function to be recalled during the loop for angle detection
-
-    EUL1 = EUL1 - Eulzero1;
-    EUL2 = EUL2 - Eulzero2;
-    NEWANGLE = EUL2 - EUL1 +180;   
-        
-end
-
